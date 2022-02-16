e62912fa2773742e141748e7edf19722c3d65128
mderka1356@yandex.ru <mderka1356@yandex.ru>
2022-02-17 01:00:08 +0500

    pz_103

---
+++ -/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace pz_18
+{
+    class Program
+    {
+        //Задание 1
+        static int z1(double a1, double d, double n1)
+        {
+
+            if (n1 < 0)
+            {
+                Console.WriteLine("Ошибка");
+                return 0;
+            }
+            if (n1 == 0)
+            {
+                Console.WriteLine(a1);
+                return (int)a1;
+            }
+            else
+            {
+                a1 = a1 + d;
+                n1 = n1-1;
+                return z1(a1, d, n1);
+            }
+
+        }
+        //Задание 2
+        static double z2(int b1, int q, int n2)
+        {
+            if (n2 < 0) {
+                Console.WriteLine("Ошибка");
+                return 0; 
+            }
+            if (n2 == 0) {
+                Console.WriteLine(b1);
+                return b1;
+            }
+            else {
+                b1 = b1 * q; 
+                n2 = n2 - 1;
+                return z2(b1, q, n2);
+            }
+
+        }
+        //Задание 3
+        static int z3(int A, int B)
+        {
+            if (A == B) {
+                Console.WriteLine($"{A}\n");
+                return 0;
+            }
+            else { 
+                Console.Write($"{A} ");
+                A += 1;
+            }
+            return z3(A, B);
+        }
+        //Задание 4
+        static bool Polyndrom(string str)
+        {
+            if (str[0] == str[^1] || str.Length <= 1)
+                return true;
+
+            Polyndrom(str.Substring(1, str.Length - 2));
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Задание №1:");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            z1(7, -0.1, n1);
+            Console.Write("Задание №2:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            z2(13, 3, n2);
+            Console.Write("задание №3:");
+            z3(8, 65);
+            Console.Write("задание №4:");
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+            Console.WriteLine(Polyndrom(str));
+            
+        }
+    }
+}
---
+++ PZ_103/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace PZ_103
+
+{
+    public class DeliveryRequest
+    {
+        public int id;
+        public DateTime requestTime;
+        public int summ;
+        public string adr;
+        public DeliveryRequest(int id, DateTime x, int n, string a)
+        {
+            this.id = id;
+            requestTime = x;
+            summ = n;
+            adr = a;
+        }
+        public DeliveryRequest(int id, int n, string a)
+        {
+            this.id = id;
+            summ = n;
+            adr = a;
+        }
+        public DeliveryRequest(int id, DateTime x, string a)
+        {
+            this.id = id;
+            requestTime = x;
+            adr = a;
+        }
+        public DeliveryRequest(int id, DateTime x, int n)
+        {
+            this.id = id;
+            requestTime = x;
+            summ = n;
+        }
+        public void GetDeliveryInfo()
+        {
+            Console.WriteLine($"id: {id};\n Request time: {requestTime};\n summ: {summ};\n adr: {adr}");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = Convert.ToInt32(Console.ReadLine());
+            DateTime requestTime = Convert.ToDateTime(Console.ReadLine());
+            int summ = Convert.ToInt32(Console.ReadLine());
+            string adr = Console.ReadLine();
+            DeliveryRequest request1 = new DeliveryRequest(id, requestTime, summ, adr);
+            request1.GetDeliveryInfo();
+            DeliveryRequest request2 = new DeliveryRequest(id, summ, adr);
+            request2.GetDeliveryInfo();
+            DeliveryRequest request3 = new DeliveryRequest(id, requestTime, adr);
+            request3.GetDeliveryInfo();
+            DeliveryRequest request4 = new DeliveryRequest(id, requestTime, summ,);
+            request4.GetDeliveryInfo();
+
+        }
+    }
+}
