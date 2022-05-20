ea3fcec35ff7954217ada6696ca5819017af22e8
mderka1356@yandex.ru <mderka1356@yandex.ru>
2022-05-21 01:09:31 +0500

    TR

---
+++ Text_redaktor/Copypasta/Copypas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Copypasta
+{
+    public class Copypas
+    {
+        
+        public string text;
+
+        public string text11 = "зритель";
+        public string text12 = "Это я - твой единственный зритель. Я на протяжении многих лет создавал иллюзию того, что тебя смотрят много людей, но это был я. Сейчас напишу это сообщение со всех аккаунтов.";
+        public string text13;
+
+        public string text21 = "бан";
+        public string text22 = "король наш погиб BibleThump Но память живет BibleThump Солнце спустилось за горизонт BibleThump Олды пришли на могилу вождя BibleThump Спи сладко, король BibleThump Мы любим тебя, небеса плачут BibleThump Плачут поля BibleThump Все олды на месте BibleThump Почти что заря BibleThump Недолго осталось BibleThump Прощай, ребятня BibleThump Сегодня идем Хоронить Короля";
+        public string text23;
+
+        public string text31 = "привет";
+        public string text32 = "П-п-привет, я тут новенькая  KonCha Я аниме девочка 17 лет, с розовыми волосами, мама не даёт денег  KonCha , так что подарите сабочку  KonCha";
+        public string text33;
+
+        public void Pasta()
+        {
+            if (text == text11)
+                text13 = text12;
+            else if (text == text21)
+                text23 = text22;
+            else if (text == text31)
+                text33 = text32;
+        }
+    }
+}
+
---
+++ Text_redaktor/Text_redaktor/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Text_redaktor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string filename = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bold_but(object sender, RoutedEventArgs e)
+        {
+            Boolean bold = false;
+            if (bold == false)
+            {
+                txt.FontWeight = FontWeights.Bold;
+                bold = true;
+            }
+            else
+            {
+                txt.FontWeight = FontWeights.Normal;
+                bold = false;
+            }
+        }
+        
+        private void italic_but(object sender, RoutedEventArgs e)
+        {
+            Boolean italic = false;
+            if (italic == false)
+            {
+                txt.FontStyle = FontStyles.Italic;
+                italic = true;
+            }
+            else
+            {
+                txt.FontStyle = FontStyles.Normal;
+                italic = false;
+            }
+        }
+
+        private void underline_but(object sender, RoutedEventArgs e)
+        {
+            Boolean underline = false;
+            if (underline == false)
+            {
+                txt.TextDecorations = TextDecorations.Underline;
+                underline = true;
+            }
+            else
+            {
+                txt.TextDecorations = TextDecorations.Baseline;
+                underline = false;
+            }
+        }
+
+        private void newFileMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CreateFileWindow createFileWindow = new CreateFileWindow();
+            if (createFileWindow.ShowDialog() == true)
+            {
+                filename = createFileWindow.FileName;
+                filesLB.Items.Add(filename);
+
+                CreateFile(filename);
+            }
+
+        }
+
+        private void CreateFile(string name)
+        {
+            
+        }
+
+
+    }
+}
